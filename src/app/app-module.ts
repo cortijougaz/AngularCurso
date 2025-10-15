f7b6dcd45ff8d537b80ed704bb0d4556044f1654
Ricardo Cortijo <cortijougaz@gmail.com>
2025-10-15 16:12:55 -0500

    Clase8: Commit inicial.

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -2,24 +2,14 @@ import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
-import { App } from './app';
+import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
-import { Ciclo } from './ciclo/ciclo';
-import { UsuarioCardComponent } from './usuario-card-component/usuario-card-component';
 import { DirectivaEjemplo } from './directiva-ejemplo';
-import { ProductosComponent } from './productos-component/productos-component';
-import { TareaForm } from './tarea/tarea-form/tarea-form';
-import { TareaList } from './tarea/tarea-list/tarea-list';
 
 @NgModule({
   declarations: [
-    App,
-    Ciclo,
-    UsuarioCardComponent,
-    DirectivaEjemplo,
-    ProductosComponent,
-    TareaForm,
-    TareaList
+    AppComponent,
+    DirectivaEjemplo
   ],
   imports: [
     BrowserModule,
@@ -29,6 +19,6 @@ import { TareaList } from './tarea/tarea-list/tarea-list';
   providers: [
     provideBrowserGlobalErrorListeners()
   ],
-  bootstrap: [App]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
